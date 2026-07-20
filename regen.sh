53280d29d68297255cc8716ede40420387ce153f
khensin1444 <wawa22arpk@gmail.com>
2026-07-21 00:06:33 +0700

    adapt into courbet

---
+++ regen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Array of device names
+devices=("courbet_user")
+
+# Loop through the devices
+for device in "${devices[@]}"
+do
+    echo "Generating defconfig for $device"
+    make ARCH=arm64 O=out "vendor/${device}"_defconfig
+    make ARCH=arm64 O=out menuconfig
+    cp out/.config "arch/arm64/configs/vendor/${device}_defconfig"
+    rm -rf out
+    echo "Defconfig generated for $device"
+    echo ""
+done
+
+echo "Defconfig generation completed for all devices."
